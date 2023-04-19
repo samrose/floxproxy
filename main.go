5149332835975dbf004b15d2a0ed6df06ea70124
Sam Rose <sam@floxdev.com>
2023-04-19 13:22:54 -0400

    even simpler

--- main.go
+++ main.go
@@ -1,47 +1,27 @@
-//borrowed from https://gist.github.com/c0d3kid/862276a6291e4aeaf2638e5f7e93a954
 package main
 
 import (
     "crypto/tls"
-    "log"
     "net/http"
     "net/http/httputil"
     "net/url"
 )
 
-type Proxy struct {
-    target *url.URL
-    proxy  *httputil.ReverseProxy
-}
-
-func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    log.Println(r.URL)
-    r.Host = p.target.Host
-    //w.Header().Set("X-Ben", "radi")
-    p.proxy.ServeHTTP(w, r)
-}
-
 func main() {
-    // Replace 'target' with the URL of the server you want to proxy to
-    target, err := url.Parse("http://localhost:3000/api/")
-    if err != nil {
-        panic(err)
-    }
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        target := r.Header.Get("X-Target")
 
-    // Create a new ReverseProxy instance
-    proxy := httputil.NewSingleHostReverseProxy(target)
+        transport := &http.Transport{
+            TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+        }
 
-    // Configure the reverse proxy to use HTTPS
-    proxy.Transport = &http.Transport{
-        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-    }
+        targetURL, _ := url.Parse(target)
+        reverseProxy := httputil.NewSingleHostReverseProxy(targetURL)
+        reverseProxy.Transport = transport
 
-    // Create a new Proxy instance
-    p := &Proxy{target: target, proxy: proxy}
+        // Serve the request using the reverse proxy.
+        reverseProxy.ServeHTTP(w, r)
+    })
 
-    // Start the HTTP server and register the Proxy instance as the handler
-    err = http.ListenAndServe(":3001", p)
-    if err != nil {
-        panic(err)
-    }
+    http.ListenAndServe(":8080", nil)
 }
