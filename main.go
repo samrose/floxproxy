8e8dbb7a58cf13435464b17030268a1db8ae3db4
Sam Rose <sam@floxdev.com>
2023-04-19 13:47:32 -0400

    cleanup

--- main.go
+++ main.go
@@ -11,17 +11,14 @@ import (
 )
 
 func main() {
-    // Create a file for logging.
     file, err := os.OpenFile("server.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
     if err != nil {
         log.Fatalf("Error opening log file: %v", err)
     }
     defer file.Close()
 
-    // Configure the logger to write to the file.
     log.SetOutput(file)
 
-    // Set the log level to Info to only log important messages.
     log.SetLevel(log.InfoLevel)
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -33,20 +30,16 @@ func main() {
             log.Infof("Incoming request: %v", string(dump))
         }
 
-        // Parse the request header to extract the "target" value.
         target := r.Header.Get("X-Target")
 
-        // Create a transport with the InsecureSkipVerify option enabled.
         transport := &http.Transport{
             TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
         }
 
-        // Create a reverse proxy with the "target" and "transport" options.
         targetURL, _ := url.Parse(target)
         reverseProxy := httputil.NewSingleHostReverseProxy(targetURL)
         reverseProxy.Transport = transport
 
-        // Serve the request using the reverse proxy.
         reverseProxy.ServeHTTP(w, r)
     })
 
